834be0ee3b833324f3c2b9bb4f5fbfbbfa21fe36
Kosuke Kimura <kosukekimura0528@gmail.com>
2019-08-09 19:49:14 +0900

    kimura_1

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,15 @@
+require 'pry'
+
+KEEPING_FEE = 0.2
+INCREASING_FEE = 0.8
+
 puts "input waiting..."
 input = gets # 標準入力を受け付ける
+times = input.split(' ').map(&:to_i)
+
+
+
+binding.pry
+
 result = input.chomp == 'answer' # inputには改行文字が含まれているためchompしています
 puts result
