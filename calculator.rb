cc77ccb754c422530fa0aac26e13b336fe607203
Kosuke Kimura <kosukekimura0528@gmail.com>
2019-08-09 20:09:38 +0900

    kimura_2

--- calculator.rb
+++ calculator.rb
@@ -7,9 +7,14 @@ puts "input waiting..."
 input = gets # 標準入力を受け付ける
 times = input.split(' ').map(&:to_i)
 
-
+time_reach = times[0]
+working_time = times.slice(1..times.length)
+increasing_time = time_reach
+keeping_time = working_time.reduce(0){|acc, cur| acc + cur } - increasing_time
+keeping_total_fee = increasing_time * INCREASING_FEE + keeping_time + KEEPING_FEE
 
 binding.pry
+
 
 result = input.chomp == 'answer' # inputには改行文字が含まれているためchompしています
 puts result
